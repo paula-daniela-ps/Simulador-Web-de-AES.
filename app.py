912187b2b24a17458a2a79bd8c36f0474547ccef
Paula Daniela <143467357+paula-daniela-ps@users.noreply.github.com>
2025-06-30 16:08:39 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Simulador AES", layout="wide")
+
+st.title("Simulador Web AES - Modos ECB e CBC")
+
+st.write("Use as abas no menu lateral para navegar entre Criptografar, Descriptografar e Comparar Desempenho.")
+
+
