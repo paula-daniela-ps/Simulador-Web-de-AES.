cd2155501e4dde59b9dfc360b3ff498c4d1d40c5
Paula Daniela <143467357+paula-daniela-ps@users.noreply.github.com>
2025-07-01 14:48:34 -0300

    Update aes_cbc.py

--- criptografia/aes_cbc.py
+++ criptografia/aes_cbc.py
@@ -1,1 +1,11 @@
-#AES
+from Crypto.Cipher import AES
+from .utils import pad, unpad
+
+def encrypt_ecb(data: bytes, chave: bytes) -> bytes:
+    cipher = AES.new(chave, AES.MODE_ECB)
+    return cipher.encrypt(pad(data))
+
+def decrypt_ecb(data: bytes, chave: bytes) -> bytes:
+    cipher = AES.new(chave, AES.MODE_ECB)
+    return unpad(cipher.decrypt(data))
+
