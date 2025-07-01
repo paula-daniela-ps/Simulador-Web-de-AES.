34ee38c73994c2b7262293649a48926190f0af96
Paula Daniela <143467357+paula-daniela-ps@users.noreply.github.com>
2025-07-01 14:50:09 -0300

    Update aes_cbc.py

--- criptografia/aes_cbc.py
+++ criptografia/aes_cbc.py
@@ -1,11 +1,13 @@
 from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 from .utils import pad, unpad
 
-def encrypt_ecb(data: bytes, chave: bytes) -> bytes:
-    cipher = AES.new(chave, AES.MODE_ECB)
-    return cipher.encrypt(pad(data))
+def encrypt_cbc(data: bytes, chave: bytes) -> tuple[bytes, bytes]:
+    iv = get_random_bytes(16)
+    cipher = AES.new(chave, AES.MODE_CBC, iv)
+    encrypted = cipher.encrypt(pad(data))
+    return encrypted, iv
 
-def decrypt_ecb(data: bytes, chave: bytes) -> bytes:
-    cipher = AES.new(chave, AES.MODE_ECB)
+def decrypt_cbc(data: bytes, chave: bytes, iv: bytes) -> bytes:
+    cipher = AES.new(chave, AES.MODE_CBC, iv)
     return unpad(cipher.decrypt(data))
-
